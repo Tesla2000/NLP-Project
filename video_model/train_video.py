9bfd3ee815674dba5d5f5dfb859906d5af2d8f87
Tesla2000 <fratajczak124@gmail.com>
2024-04-22 15:09:46 +0200

    Updated

--- video_model/train_video.py
+++ video_model/train_video.py
@@ -3,8 +3,9 @@ from itertools import count
 from math import ceil
 from pathlib import Path
 
+import numpy as np
 import torch
-from keras.src.metrics import accuracy
+from sklearn.metrics import accuracy_score
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -17,16 +18,16 @@ from video_model.VideoModel import VideoModel
 
 
 def train_video():
+    batch_size = Config.batch_size
     model = VideoModel(image_model=efficientnet_b0(), n_classes=Config.n_classes).to(
         Config.device
     )
     loss_function = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters())
     train_dataset = VideoDataset(Config.train_video_path, Config.train_path)
-    train_dataloader = DataLoader(train_dataset)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
     val_dataset = VideoDataset(Config.val_video_path, Config.val_path)
-    val_dataloader = DataLoader(val_dataset)
-    batch_size = Config.batch_size
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
     total = ceil(len(train_dataset) / batch_size)
     best_accuracy = 0
     best_loss = float("inf")
@@ -34,7 +35,7 @@ def train_video():
     consecutive_lack_of_improvement = 0
     for epoch in count():
         model.train()
-        for frames, label in tqdm(
+        for frames, labels in tqdm(
             train_dataloader,
             total=total,
             desc=f"Previous {accuracy=:.4f}, {new_loss=:.4f}. Training epoch {epoch}..."
@@ -42,19 +43,26 @@ def train_video():
             else f"Training epoch {epoch}...",
         ):
             optimizer.zero_grad()
-            output = model(frames)
-            loss = loss_function(output, label)
+            outputs = model(frames)
+            loss = loss_function(outputs, labels)
             loss.backward()
             optimizer.step()
         model.eval()
         new_loss = 0.0
-        for frames, label in val_dataloader:
+        label_ids = []
+        predictions = []
+        for frames, labels in val_dataloader:
+            label_ids += list(labels.to("cpu").numpy())
             with torch.no_grad():
                 optimizer.zero_grad()
-                output = model(frames)
-                loss = loss_function(output, label)
+                outputs = model(frames)
+                loss = loss_function(outputs, labels)
+                predictions += list(outputs)
                 new_loss += loss.item()
                 optimizer.step()
+        accuracy = accuracy_score(
+            np.argmax(label_ids, axis=1), np.argmax(predictions, axis=1)
+        )
         if best_loss <= new_loss:
             consecutive_lack_of_improvement += 1
             if (
