4512e794a4256eb23ccc9f4fb3a21eb6ba790d7a
Filip Ratajczak <90644163+Tesla2000@users.noreply.github.com>
2024-05-17 11:28:00 +0200

    Update AudioSpectrogramModel.py

--- audio_spectrogram_model/AudioSpectrogramModel.py
+++ audio_spectrogram_model/AudioSpectrogramModel.py
@@ -6,7 +6,7 @@ class AudioSpectrogramModel(nn.Module):
     def __init__(self, num_classes: int):
         super(AudioSpectrogramModel, self).__init__()
 
-        self.resnet = resnet18(pretrained=True)
+        self.resnet = resnet18()
 
         self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), 
                                       padding=(3, 3), bias=False)
