3d05896372bde2b751be58da1edb01c5b5875839
Tesla2000 <fratajczak124@gmail.com>
2024-06-01 17:51:49 +0200

    unsqueezed spectrogram

--- src/audio_spectrogram_model/AudioSpectrogramDataset.py
+++ src/audio_spectrogram_model/AudioSpectrogramDataset.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from ..combinations.LabelsDataset import LabelsDataset
@@ -15,9 +16,9 @@ class AudioSpectrogramDataset(LabelsDataset):
         label = self.sentiment_to_label[sentiment]
         try:
             spectrogram_path = self.spectrograms_path.joinpath(self.files[index])
-            spectrogram = pd.read_parquet(
-                spectrogram_path.with_suffix(".parquet")
-            ).to_numpy()
+            spectrogram = np.array(
+                [pd.read_parquet(spectrogram_path.with_suffix(".parquet")).to_numpy()]
+            )
         except OSError as e:
             print(e)
             return self.__getitem__(index - 1)
